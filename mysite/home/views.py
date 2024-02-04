4192efebaff808c342b9036123a9068ee27b5139
cainmo <cainomomo@gmail.com>
2024-02-04 04:43:39 -0800

    Add responses to main.html

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -6,8 +6,10 @@ from prompts.models import *
 
 def main(request):
     question_list = Question.objects.all()
+    randPrompt = random.choice(question_list)
     context = {
-        "prompt": random.choice(question_list),
+        "prompt": randPrompt,
         "respPostURL": request.build_absolute_uri('/'),
+        "responses": Response.objects.filter(question_id=randPrompt.id),
     }
     return render(request, "home/main.html", context)

c1daaf726022004d042f1436a86a1a8cb74e5cda
kmachnickiwork49 <37753492+kmachnickiwork49@users.noreply.github.com>
2024-02-03 20:51:27 -0800

    Update index.html to make circles out of letter input

---
+++ src/GravSim.tsx
@@ -0,0 +1,17 @@
+function GravSim() {
+    return (
+        <div className="GravSim">
+            <head>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+
+                <script type="text/javascript">
+
+                </script>
+            </head>
+            <body>
+            </body>
+        </div>
+    );
+  }
+
+export default GravSim;
